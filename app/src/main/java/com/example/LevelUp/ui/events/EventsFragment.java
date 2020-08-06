78b5eeef3dba07aa787c7b676d9bed51e7de2179
yienyoong <yoong.yi.en.2000@gmail.com>
2020-08-06 15:30:49 +0800

    UI Background Changes

--- app/src/main/java/com/example/LevelUp/ui/events/EventsFragment.java
+++ app/src/main/java/com/example/LevelUp/ui/events/EventsFragment.java
@@ -90,7 +90,7 @@ public class EventsFragment extends Fragment {
 
         buildRecyclerView();
         floatingActionButton = rootView.findViewById(R.id.fab);
-        if (MainActivity.getCurrentUser().getIsStaff()) {
+        if (MainActivity.getCurrentUser() != null && MainActivity.getCurrentUser().getIsStaff()) {
             floatingActionButton.setAlpha(0.50f); // setting transparency
             floatingActionButton.setOnClickListener(new View.OnClickListener() {
                 @Override
