2785edc914dd62366d39f26c72f9c5d04465f842
Keith Low <keithlow98@gmail.com>
2021-08-17 23:39:32 +0800

    Improve variable names in DashboardAdapter

--- app/src/main/java/com/Dashboard/LevelUp/ui/dashboard/DashboardAdapter.java
+++ app/src/main/java/com/Dashboard/LevelUp/ui/dashboard/DashboardAdapter.java
@@ -31,11 +31,11 @@ import androidx.recyclerview.widget.RecyclerView;
 public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.DashboardViewHolder> {
 
     private ArrayList<Occasion> occasionList;
-    private StorageReference mProfileStorageRef;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mUserRef;
+    private StorageReference profileStorageRef;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference userRef;
     private DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.UK);
-    private FragmentActivity mContext;
+    private FragmentActivity fragContext;
 
     public static class DashboardViewHolder extends RecyclerView.ViewHolder {
         private String creatorUid;
@@ -51,11 +51,11 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
         private boolean isChecked;
         private boolean isLiked;
         private int numLikes;
-        private ImageView mImageView;
-        private TextView mTextView1;
-        private TextView mTextView3;
-        private TextView mTextView4;
-        private TextView mTextView5;
+        private ImageView imageView;
+        private TextView titleView;
+        private TextView dateView;
+        private TextView locationView;
+        private TextView timeView;
 
         /**
          * Constructor for the DashboardViewHolder class
@@ -65,11 +65,11 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
          */
         public DashboardViewHolder (final Context context, View itemView) {
             super(itemView);
-            mImageView = itemView.findViewById(R.id.category_imageView);
-            mTextView1 = itemView.findViewById(R.id.title);
-            mTextView3 = itemView.findViewById(R.id.date);
-            mTextView4 = itemView.findViewById(R.id.location);
-            mTextView5 = itemView.findViewById(R.id.time);
+            imageView = itemView.findViewById(R.id.category_imageView);
+            titleView = itemView.findViewById(R.id.title);
+            dateView = itemView.findViewById(R.id.date);
+            locationView = itemView.findViewById(R.id.location);
+            timeView = itemView.findViewById(R.id.time);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -77,11 +77,11 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
                     Intent intent = new Intent(context, EventPage.class);
                     intent.putExtra("uid", creatorUid);
                     intent.putExtra("creatorName", creatorName);
-                    intent.putExtra("title", mTextView1.getText().toString());
+                    intent.putExtra("title", titleView.getText().toString());
                     intent.putExtra("description", description);
-                    intent.putExtra("date", mTextView3.getText().toString());
-                    intent.putExtra("location", mTextView4.getText().toString());
-                    intent.putExtra("time", mTextView5.getText().toString());
+                    intent.putExtra("date", dateView.getText().toString());
+                    intent.putExtra("location", locationView.getText().toString());
+                    intent.putExtra("time", timeView.getText().toString());
                     intent.putExtra("position", getAdapterPosition());
                     intent.putExtra("stateChecked", isChecked);
                     intent.putExtra("stateLiked", isLiked);
@@ -152,11 +152,11 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
      */
     public DashboardAdapter(FragmentActivity context, ArrayList<Occasion> occasionList) {
         this.occasionList = occasionList;
-        mProfileStorageRef = FirebaseStorage.getInstance()
+        profileStorageRef = FirebaseStorage.getInstance()
                 .getReference("profile picture uploads");
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mUserRef = mFirebaseDatabase.getReference("Users");
-        this.mContext = context;
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        userRef = firebaseDatabase.getReference("Users");
+        this.fragContext = context;
     }
 
 
@@ -164,49 +164,49 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
     @Override
     public DashboardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.occ_item_dashboard, parent, false);
-        DashboardAdapter.DashboardViewHolder dvh = new DashboardAdapter.DashboardViewHolder(mContext, v);
+        DashboardAdapter.DashboardViewHolder dvh = new DashboardAdapter.DashboardViewHolder(fragContext, v);
         return dvh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull DashboardViewHolder holder, final int position) {
         final Occasion currentItem = occasionList.get(position);
-        final DashboardAdapter.DashboardViewHolder holder1 = holder;
+        final DashboardAdapter.DashboardViewHolder viewHolder = holder;
         final String creatorUid = currentItem.getCreatorID();
-        holder1.setCreatorUid(creatorUid);
+        viewHolder.setCreatorUid(creatorUid);
 
         int category = currentItem.getCategory();
 
         if (category == 0) { // arts
-            holder1.mImageView.setImageResource(R.drawable.arts);
+            viewHolder.imageView.setImageResource(R.drawable.arts);
         }
         if (category == 1) { // sports
-            holder1.mImageView.setImageResource(R.drawable.sports);
+            viewHolder.imageView.setImageResource(R.drawable.sports);
         }
         if (category == 2) { // talks
-            holder1.mImageView.setImageResource(R.drawable.talks);
+            viewHolder.imageView.setImageResource(R.drawable.talks);
         }
         if (category == 3) { // volunteering
-            holder1.mImageView.setImageResource(R.drawable.volunteering);
+            viewHolder.imageView.setImageResource(R.drawable.volunteering);
         }
         if (category == 4) { // food
-            holder1.mImageView.setImageResource(R.drawable.food);
+            viewHolder.imageView.setImageResource(R.drawable.food);
         }
         if (category == 5) { // food
-            holder1.mImageView.setImageResource(R.drawable.others);
+            viewHolder.imageView.setImageResource(R.drawable.others);
         }
 
-        holder1.mTextView1.setText(currentItem.getTitle());
-        holder1.description = currentItem.getDescription();
-        holder1.mTextView4.setText(currentItem.getLocationInfo());
+        viewHolder.titleView.setText(currentItem.getTitle());
+        viewHolder.description = currentItem.getDescription();
+        viewHolder.locationView.setText(currentItem.getLocationInfo());
 
         String date = df.format(currentItem.getDateInfo());
-        holder1.mTextView3.setText(date);
+        viewHolder.dateView.setText(date);
 
         String time = currentItem.getTimeInfo();
-        holder1.mTextView5.setText(time);
+        viewHolder.timeView.setText(time);
 
-        mUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
@@ -220,12 +220,12 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
                         String email = selected.getEmail();
                         String dpUri = selected.getProfilePictureUri();
                         long phone = selected.getPhone();
-                        holder1.setCreatorName(name);
-                        holder1.setCreatorResidence(res);
-                        holder1.setTelegram(telegram);
-                        holder1.setEmail(email);
-                        holder1.setProfilePictureUri(dpUri);
-                        holder1.setPhone(phone);
+                        viewHolder.setCreatorName(name);
+                        viewHolder.setCreatorResidence(res);
+                        viewHolder.setTelegram(telegram);
+                        viewHolder.setEmail(email);
+                        viewHolder.setProfilePictureUri(dpUri);
+                        viewHolder.setPhone(phone);
                     }
                 }
             }
@@ -237,25 +237,25 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
         });
 
         final String eventID = currentItem.getOccasionID();
-        holder1.setEventID(eventID);
+        viewHolder.setEventID(eventID);
 
         // set stateChecked and stateLiked to pass into the intent
         if (currentItem.isJio()) {
             if (MainActivity.getJioIds().contains(currentItem.getOccasionID())) {
-                holder1.setChecked(true);
+                viewHolder.setChecked(true);
             }
             if (MainActivity.getLikeJioIDs().contains(currentItem.getOccasionID())) {
-                holder1.setLiked(true);
+                viewHolder.setLiked(true);
             }
         } else {
             if (MainActivity.getEventIDs().contains(currentItem.getOccasionID())) {
-                holder1.setChecked(true);
+                viewHolder.setChecked(true);
             }
             if (MainActivity.getLikeEventIDs().contains(currentItem.getOccasionID())) {
-                holder1.setLiked(true);
+                viewHolder.setLiked(true);
             }
         }
-        holder1.setNumLikes(currentItem.getNumLikes());
+        viewHolder.setNumLikes(currentItem.getNumLikes());
     }
 
     @Override
