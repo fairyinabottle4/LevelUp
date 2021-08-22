602bdab478d49a89a3463a38cf5ac27ba60ed9d8
Keith Low <keithlow98@gmail.com>
2021-08-23 00:50:21 +0800

    Add instrumentation test for EventsFragment class

--- app/src/androidTest/java/com/example/LevelUp/ui/events/EventsFragmentTest.java
+++ app/src/androidTest/java/com/example/LevelUp/ui/events/EventsFragmentTest.java
@@ -5,7 +5,6 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
 
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.lifecycle.Lifecycle;
