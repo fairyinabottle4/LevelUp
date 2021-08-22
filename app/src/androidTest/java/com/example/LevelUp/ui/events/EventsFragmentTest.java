ace3dcc0adf7bd954bf4cad9b41acd59cb2c80ac
Keith Low <keithlow98@gmail.com>
2021-08-23 00:48:35 +0800

    Add instrumentation test for EventsFragment class

---
+++ app/src/androidTest/java/com/example/LevelUp/ui/events/EventsFragmentTest.java
@@ -0,0 +1,34 @@
+package com.example.LevelUp.ui.events;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.lifecycle.Lifecycle;
+
+import com.example.tryone.R;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventsFragmentTest {
+    private FragmentScenario<EventsFragment> scenario;
+
+    @Before
+    public void setup() {
+
+        scenario = FragmentScenario.launchInContainer(EventsFragment.class);
+        scenario.moveToState(Lifecycle.State.STARTED);
+    }
+
+    @Test
+    public void floatingActionButtonTest() {
+        onView(withId(R.id.fab))
+            .perform(click());
+        onView(withId(R.layout.events_adder)).check(matches(isDisplayed()));
+    }
+}
